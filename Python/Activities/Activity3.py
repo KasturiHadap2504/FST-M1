cf42099792cea2c6a265f83d1150cef9aa33cf74
KasturiHadap2504 <97496580+KasturiHadap2504@users.noreply.github.com>
2022-01-16 22:39:21 +0530

    Add files via upload
    Python activity are uploaded

---
+++ Python/Activities/Activity3.py
@@ -0,0 +1,27 @@
+user1 = input("What is Player 1's name? ")
+user2 = input("What is Player 2's name? ")
+
+
+user1_ans = input(user1 + ", do you want to choose rock, paper or scissors? ").lower()
+user2_ans = input(user2 + ", do you want to choose rock, paper or scissors? ").lower()
+
+
+if user1_ans == user2_ans:
+    print("It's a tie!")
+elif user1_ans == 'rock':
+    if user2_ans == 'scissors':
+        print("Rock wins!")
+    else:
+        print("Paper wins!")
+elif user1_ans == 'scissors':
+    if user2_ans == 'paper':
+        print("Scissors win!")
+    else:
+        print("Rock wins!")
+elif user1_ans == 'paper':
+    if user2_ans == 'rock':
+        print("Paper wins!")
+    else:
+        print("Scissors win!")
+else:
+    print("Invalid input! You have not entered rock, paper or scissors, try again.")
